158d11320f6d705f7bfbbf4bdff8f0b91fb9e12d
G-HRUSHIKESH-RAJ-SAI <80945900+G-HRUSHIKESH-RAJ-SAI@users.noreply.github.com>
2021-06-06 20:34:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReactJS Bootcamp</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha. This was a free
+        7-days bootcamp on basic web development. We covered everything from
+        scratch related to web development including Javascript,HTML and React.js.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
